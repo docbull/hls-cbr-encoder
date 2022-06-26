bc8d1c240db02de037e9147ac5f09e8eb95b73ff
docbull <fudghks21@gmail.com>
2022-06-27 01:23:02 +0900

    added more options for HLS CBR encoding

--- encoder.sh
+++ encoder.sh
@@ -1,8 +1,8 @@
 export VIDEO_FILE=$1
 
-export BITRATE="1"
+export BITRATE="8M"
 export FPS=60
-export BUFFER=3
+export BUFFER=30
 
 mkdir hls-${VIDEO_FILE}
 
@@ -11,10 +11,26 @@ cd hls-${VIDEO_FILE}
 ffmpeg -i ../${VIDEO_FILE}.mp4 \
 -c:v libx264 \
 -x264opts keyint=${FPS}:min-keyint=${FPS}:no-scenecut \
--x264-params "nal-hrd=cbr" -b:v ${BITRATE}M -maxrate:v ${BITRATE}M -minrate:v ${BITRATE}M -bufsize:v ${BUFFER}M \
--profile:v baseline \
--level 3.0 \
+-x264-params "nal-hrd=cbr:force-cfr=1" -b:v ${BITRATE} -maxrate:v ${BITRATE} -minrate:v ${BITRATE} -bufsize:v ${BUFFER}M \
+-preset slow \
+-g 48 \
+-sc_threshold 0 \
+-keyint_min 48 \
 -start_number 0 \
 -hls_time 2 \
+-hls_playlist_type vod \
+-hls_flags independent_segments \
+-hls_segment_type mpegts \
 -hls_list_size 0 \
 -f hls master.m3u8
+
+# ffmpeg -i ../${VIDEO_FILE}.mp4 \
+# -c:v libx264 \
+# -x264opts keyint=${FPS}:min-keyint=${FPS}:no-scenecut \
+# -x264-params "nal-hrd=cbr" -b:v ${BITRATE} -maxrate:v ${BITRATE} -minrate:v ${BITRATE} -bufsize:v ${BUFFER}M \
+# -profile:v baseline \
+# -level 3.0 \
+# -start_number 0 \
+# -hls_time 2 \
+# -hls_list_size 0 \
+# -f hls master.m3u8
